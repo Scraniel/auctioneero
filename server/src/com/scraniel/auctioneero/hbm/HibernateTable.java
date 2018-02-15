f69295236b07db530d4c2fdc2cfd31dfb99ef4dd
Danny Lewis <nitomatik@gmail.com>
2018-02-15 00:44:39 -0800

    Adding update and delete to auction item

--- server/src/com/scraniel/auctioneero/hbm/HibernateTable.java
+++ server/src/com/scraniel/auctioneero/hbm/HibernateTable.java
@@ -17,6 +17,11 @@ public abstract class HibernateTable
 	@Column(name = "id", length = 36)
 	protected UUID id;
 	
+	// The session used to retrieve this object
+	//
+	@Transient
+	protected Session currentSession;
+	
 	public UUID getId() 
 	{
 		return id;
@@ -27,8 +32,18 @@ public abstract class HibernateTable
 		this.id = id;
 	}
 	
+	public Session getCurrentSession() 
+	{
+		return currentSession;
+	}
+	
+	public void setCurrentSession(Session currentSession) 
+	{
+		this.currentSession = currentSession;
+	}
+	
 	/**
-	 * Returns an informational message when this object gets inserted to it's table
+	 * Returns an informational message when this object gets inserted to its table
 	 * 
 	 * TODO: May want to have these format string (among other strings) to a config file
 	 * 
@@ -37,13 +52,23 @@ public abstract class HibernateTable
 	protected abstract String getSuccessfulInsertMessage();
 	
 	/**
-	 * Performs a simple insert of the current object (ie. start transaction, save, commit transaction
+	 * Returns an informational message when this object gets deleted from its table
+	 * 
+	 * TODO: May want to have these format string (among other strings) to a config file
+	 * 
+	 * @return String with informational message upon successful delete
+	 */
+	protected abstract String getSuccessfulDeleteMessage();
+	
+	/**
+	 * Performs a simple insert of the current object (ie. start transaction, save, commit transaction)
 	 * 
 	 * @return AuctionResponse object either containing error or the UUID of the newly inserted object
 	 */
 	public AuctionResponse insert()
 	{
 		// Required so anonymous class uses subclass instead of parent
+		//
 		HibernateTable current = this;
 		
 		HibernateSqlStatement statement = new HibernateSqlStatement()
@@ -59,5 +84,31 @@ public abstract class HibernateTable
 		
 		return HibernateContext.getInstance().executeSqlStatement(statement);
 	}
-
+	
+	/**
+	 * Performs a simple delete of the current object (ie. start transaction, delete, commit transaction)
+	 * Should only be called with a persistent instance (if you have not-null columns, cannot delete from 
+	 * transient instance)
+	 * 
+	 * @return AuctionResponse object either containing error or the successful delete message
+	 */
+	public AuctionResponse delete()
+	{
+		// Required so anonymous class uses subclass instead of parent
+		//
+		HibernateTable current = this;
+		
+		HibernateSqlStatement statement = new HibernateSqlStatement()
+		{			
+			@Override
+			public AuctionResponse execute(Session session) throws HibernateException
+			{
+				session.delete(current);
+				
+				return new AuctionResponse(true, getSuccessfulDeleteMessage(), null);
+			}
+		};
+		
+		return HibernateContext.getInstance().executeSqlStatement(statement, currentSession);
+	}
 }
--- server/src/com/scraniel/auctioneero/tables/AuctionItem.java
+++ server/src/com/scraniel/auctioneero/tables/AuctionItem.java
@@ -8,8 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import org.hibernate.HibernateException;
+import org.hibernate.LockOptions;
+import org.hibernate.Session;
 import org.hibernate.annotations.Type;
 
+import com.scraniel.auctioneero.AuctionResponse;
+import com.scraniel.auctioneero.hbm.HibernateContext;
+import com.scraniel.auctioneero.hbm.HibernateSqlStatement;
 import com.scraniel.auctioneero.hbm.HibernateTable;
 
 @Entity
@@ -35,6 +41,7 @@ public class AuctionItem extends HibernateTable
 	private UUID highBidderId;
 	
 	// We use custom getter setter to store as VARCHAR (see getCategoryString / setCategoryString)
+	@Transient
 	private Category category;
 	
 	@Column(name = "expiry", nullable = false)
@@ -130,4 +137,72 @@ public class AuctionItem extends HibernateTable
 		return String.format("The item '%s' has been added with initial bid %.2f", name, currentBid);
 	}
 	
+	@Override
+	protected String getSuccessfulDeleteMessage() 
+	{
+		return "Item deleted successfully.";
+	}
+	
+	/**
+	 * Bids on an item. Should only be called with a persistent instance.
+	 * 
+	 * @param bid The new bid for the item
+	 * @param bidderId The bidder
+	 * @return
+	 */
+	public AuctionResponse bidOn(float bid, UUID bidderId)
+	{
+		if(currentSession == null)
+		{
+			// TODO: Should be exception
+			//
+			return new AuctionResponse(false, "Need persistent instance", null);
+		}
+		
+		HibernateSqlStatement statement = new HibernateSqlStatement()
+		{
+			@Override
+			public AuctionResponse execute(Session session) throws HibernateException
+			{
+				String message = null;
+				boolean isSuccessfulBid = true;
+				Timestamp now = new Timestamp(System.currentTimeMillis());
+				
+				// 2. Check the expiry and price, abort if too high. Don't want to do fully in SQL so we can return current bid
+				//
+				if(now.after(getExpiry()))
+				{
+					isSuccessfulBid = false;
+					message = String.format("Your bid of %f could not be made; auction expired on %f", bid, getExpiry());
+				}
+				else if(getCurrentBid() >= bid)
+				{
+					// TODO: Extract messages to config
+					//
+					isSuccessfulBid = false;
+					message = String.format("Your bid of %f could not be made; the current high bid is %f", bid, getCurrentBid());
+					
+					// 2.1. Unlock the row
+					// END;
+					//
+				}
+				else
+				{
+					// 3. Update the item, unlock
+					// END;
+					//
+					// Note that since we used session.load, the item is in a persistent state and is tied to the session. Thus,
+					// the item will be persisted as soon as the transaction is committed, no save / merge / update necessary.
+					//
+					setCurrentBid(bid);
+					setHighBidderId(bidderId);
+					message = String.format("Your bid of %f is the new highest bid", bid);
+				}	
+				
+				return new AuctionResponse(isSuccessfulBid, message, getId());
+			}
+		};
+		
+		return HibernateContext.getInstance().executeSqlStatement(statement, currentSession); 
+	}
 }
